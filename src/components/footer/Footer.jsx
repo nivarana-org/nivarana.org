db4316fff47432420fa2696273cdcff27f949b87
Akshay S Dinesh <akshay@learnlearn.in>
2024-10-21 00:18:27 +0530

    fix: allow wrap

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,9 +4,9 @@ import { NivaranaSocialMedia } from "../social";
 const Footer = () => {
     return (
         <footer>
-            <div className='p-2 flex space-x-4 items-center mb-6 justify-evenly'>
-                <div className="mr-2">© 2024 Nivarana. All Rights Reserved.</div>
-                <div className="flex space-x-4">
+            <div className='p-2 flex flex-wrap space-x-4 items-center mb-6 justify-evenly'>
+                <div className="mr-2 mb-5">© 2024 Nivarana. All Rights Reserved.</div>
+                <div className="flex space-x-4 mb-5">
                     <Link href="/contact">Contact Us</Link>
                     <Link href="/about">About Us</Link>
                     <Link href="/submission-guidelines">Submission Guidelines</Link>
