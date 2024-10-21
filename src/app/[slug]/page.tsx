fcc270d41eb0a8bead8158c15558c008b3c1f99e
Akshay S Dinesh <akshay@learnlearn.in>
2024-10-21 15:55:07 +0530

    feat: random quote on standalone page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,20 @@
-export default async function Page({ params }: { params: { slug: string } }) {
-    return <>
-        <h1>That is a standalone page</h1>
-        <p>
-        </p >
-    </>
+'use client'
+
+import { useEffect, useState } from "react";
+
+export default function Page({ params }: { params: { slug: string } }) {
+    const [quote, setQuote] = useState<{quote: string, author: string}>();
+
+    useEffect(() => {
+        fetch('https://quotes-api-self.vercel.app/quote')
+            .then(d => d.json())
+            .then(q => setQuote(q));
+    }, [])
+
+    if (!quote) return;
+
+    return <div className="max-w-prose mx-auto">
+        <h1>&quot;{quote?.quote}&quot;</h1>
+        <h2 className="ml-40">{quote?.author}</h2>
+    </div>
 }
