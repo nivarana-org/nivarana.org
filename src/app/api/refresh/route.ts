8e915bc6190a5561149b73a17e879420129f738b
Akshay S Dinesh <akshay@learnlearn.in>
2024-10-27 13:56:26 +0530

    feat: allow for clients to decide what to revalidate

--- src/app/api/refresh/route.ts
+++ src/app/api/refresh/route.ts
@@ -1,6 +1,24 @@
 import { revalidatePath } from "next/cache"
+import { NextRequest } from "next/server";
 
-export async function GET(request: Request) {
-    revalidatePath('/');
-    return Response.json({"done": new Date()})
+const invalidate = (path: string, type?: "layout" | "page") => {
+  revalidatePath(path, type);
+  return Response.json({"done": new Date()})
+}
+
+export async function GET(request: NextRequest) {
+    const key = request.nextUrl.searchParams.get('key')
+    if (key !== process.env.REVALIDATE_KEY) {
+      return Response.json({"done": false});
+    }
+    const path = request.nextUrl.searchParams.get('path') || '/'
+    const type = request.nextUrl.searchParams.get('type')
+    if (type === null) {
+      return invalidate(path)
+    }
+    if (!(type === "layout" || type === "page")) return Response.json({
+      done: "false",
+      reason: `Invalid type ${type}`
+    })
+    return invalidate(path, type);
   }
