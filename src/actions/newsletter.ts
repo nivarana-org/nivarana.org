66f58c6a3610c1b788daa3487ad9f5c84bb05708
Akshay S Dinesh <akshay@learnlearn.in>
2024-10-31 23:46:39 +0530

    fix: unintentionally client side

--- src/actions/newsletter.ts
+++ src/actions/newsletter.ts
@@ -1,4 +1,9 @@
+"use server"
 import { addNewsLetterSubscriber } from "@/data/cms";
+
+function isKnexError(error: unknown): error is { code: string } {
+    return error !== null && typeof error === "object" && "code" in error;
+}
 
 export const addNewsLetterSubscriberAction = async (prevState: { message: string }, formData: FormData) => {
     const email = formData.get('email') || "";
@@ -8,7 +13,9 @@ export const addNewsLetterSubscriberAction = async (prevState: { message: string
         return { message: "Added successfully" };
     }
     catch (e) {
-        if (e.code = "ER_DUP_ENTRY") return { error: "Email already subscribed" };
+        if (isKnexError(e)) {
+            if (e?.code && e?.code === "ER_DUP_ENTRY") return { error: "Email already subscribed" };
+        }
         return { error: "Couldn't add the email" };
     }
 }
