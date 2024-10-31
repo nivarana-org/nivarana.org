bf8e924b0b157b4b2ec7885c270329fba8882eba
Akshay S Dinesh <akshay@learnlearn.in>
2024-10-31 23:27:53 +0530

    fix invalid properties

--- src/components/article/ViewCount.tsx
+++ src/components/article/ViewCount.tsx
@@ -4,7 +4,7 @@ import { incrementBlogViewCount } from "@/network/api";
 import { useEffect, useState } from "react";
 
 const CountIcon = () => <span>
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye">
         <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
         <circle cx="12" cy="12" r="3"></circle>
     </svg>
