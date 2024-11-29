8392475108bb94d133825540f702205770836273
Akshay S Dinesh <akshay@learnlearn.in>
2024-11-29 12:32:18 +0530

    use yesterday I learnt fact

--- src/app/(admin)/admin/articles/[id]/page.tsx
+++ src/app/(admin)/admin/articles/[id]/page.tsx
@@ -3,7 +3,7 @@ import { getArticleFull } from "@/data/cms";
 
 export default async function Page(props: Props) {
     const params = await props.params;
-    const post = await getArticleFull(parseInt(params.id));
+    const post = await getArticleFull(Number(params.id));
     return (
         <div>
             <div className="text-bold">{post?.page_title}</div>
