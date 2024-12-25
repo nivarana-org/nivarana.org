7a401e56f1e1ab7eeb30de637b0a72bd0d919d07
Akshay S Dinesh <akshay@learnlearn.in>
2024-12-25 19:14:47 +0530

    feat: refactor for clarity and possibly less bugs

--- src/components/home/LoadMore.tsx
+++ src/components/home/LoadMore.tsx
@@ -4,65 +4,76 @@ import ArticlePreview from "../article/ArticlePreview";
 import { getPostsOfPage } from "@/network/api";
 import { Post } from "@/types/nivarana";
 
-export default function LoadMore({ }) {
-    const [page, setPage] = useState(2);
-    const [posts, setPosts] = useState<Post[]>([]);
-    const [hasMore, setHasMore] = useState(true);
-    const [loading, setLoading] = useState(false);
-    const [timer, setTimer] = useState<NodeJS.Timeout>();
-    const loaderRef = useRef(null);
-    const loadMore = async () => {
-        setLoading(true);
-        const morePosts = await getPostsOfPage(page);
-        if (morePosts.length > 0) {
-            setPosts([...posts, ...morePosts])
-            setPage(page + 1);
-            setLoading(false);
-        } else {
-            setHasMore(false);
-        }
+const usePosts = (startPage: number) => {
+  const [page, setPage] = useState(startPage);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const loadMore = async () => {
+    if (!hasMore) return;
+    if (loading) return;
+    setLoading(true);
+    const morePosts = await getPostsOfPage(`${page}`);
+    if (morePosts.length > 0) {
+      setPosts((posts) => [...posts, ...morePosts])
+      setPage((page) => page + 1);
+      setLoading(false);
+    } else {
+      setHasMore(false);
     }
+  }
+  const loadDelayed = () => {
+    const timer = setTimeout(() => {
+      loadMore();
+    }, 3000)
+    return timer;
+  }
 
-    const loadDelayed = () => {
-        const timer = setTimeout(() => {
-            loadMore();
-        }, 3000)
-        return timer;
-    }
-
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-          (entries) => {
-            const first = entries[0];
-            if (first.isIntersecting && hasMore && !loading) {
-              const timerId = loadDelayed();
-              setTimer(timerId)
-            }
-          },
-          {
-            threshold: 1.0,
-            rootMargin: '0px'
-          }
-        );
-
-        if (loaderRef.current) {
-          observer.observe(loaderRef.current);
-        }
-
-        return () => observer.disconnect();
-      }, [hasMore, loading, page]);
-    return <>
-        {posts.map((item) => (
-            <ArticlePreview {...item} key={item.path} />
-        ))}
-        <div ref={loaderRef} className="flex flex-col items-center">
-            <LoadButton loading={loading} loadMore={loadMore} hasMore={hasMore} />
-        </div>
-    </>
+  return {
+    posts,
+    hasMore,
+    loading,
+    loadMore,
+    loadDelayed
+  }
 }
 
-function LoadButton({loading, hasMore, loadMore}) {
-    if (!hasMore) return;
-    if (loading) return <div>Loading...</div>
-    return <button onClick={loadMore}>Load More</button>
+export default function LoadMore({}) {
+  const {posts, hasMore, loading, loadMore, loadDelayed} = usePosts(2);
+  const loaderRef = useRef(null);
+  
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const first = entries[0];
+        if (first.isIntersecting) {
+          loadDelayed();
+        }
+      },
+      {
+        threshold: 1.0,
+        rootMargin: '0px'
+      }
+    );
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+    return () => {
+      observer.disconnect()
+    };
+  }, [loadDelayed]);
+  return <>
+    {posts.map((item) => (
+      <ArticlePreview {...item} key={item.path} />
+    ))}
+    <div ref={loaderRef} className="flex flex-col items-center">
+      <LoadButton loading={loading} loadMore={loadMore} hasMore={hasMore} />
+    </div>
+  </>
+}
+
+function LoadButton({ loading, hasMore, loadMore }: {loading: boolean, hasMore: boolean, loadMore: () => void}) {
+  if (!hasMore) return;
+  if (loading) return <div>Loading...</div>
+  return <button onClick={loadMore}>Load More</button>
 }
