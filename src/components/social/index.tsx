471aa8ecec8d2b416ddac7c2dd2432212cebbd23
Akshay S Dinesh <akshay@learnlearn.in>
2024-12-25 12:06:20 +0530

    fix: remove unnecessary text in link

--- src/components/social/index.tsx
+++ src/components/social/index.tsx
@@ -8,7 +8,7 @@ export const NivaranaSocialMedia = () => {
             <a target="_blank" href="https://youtube.com/@nivarana4India?feature=shared" className="href"><YouTube /></a>
             <a target="_blank" href="https://open.spotify.com/show/3We8DodIwKwylMHZieNtAz?si=hDsy47dsQZalmmtklv5S4A" className="href"><Spotify /></a>
             <a target="_blank" href="https://open.spotify.com/show/2zfhTXNsaV26xft0NRLGnM?si=G-YNeTlHSva6zM-GjsvaNg" className="href"><Spotify /></a>
-            <a target="_blank" href="https://in.linkedin.com/company/nivaranaforindia#:~:text=About%20us,to%20health%20and%20social%20problems" className="href"><LinkedIn /></a>
+            <a target="_blank" href="https://in.linkedin.com/company/nivaranaforindia" className="href"><LinkedIn /></a>
             <a target="_blank" href="https://whatsapp.com/channel/0029Vb1kL5V9xVJjvHk25V0u" className="href"><WhatsApp /></a>
         </>
     )
