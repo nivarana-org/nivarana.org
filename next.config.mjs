895f5e44301982529210616312cc7193076561bc
Akshay S Dinesh <akshay@learnlearn.in>
2025-02-01 20:14:02 +0530

    fix: need to add config even if same URL

--- next.config.mjs
+++ next.config.mjs
@@ -49,6 +49,11 @@ const nextConfig = {
                 protocol: 'http',
                 hostname: 'localhost',
                 port: '8000'
+            },
+            {
+                protocol: 'https',
+                hostname: 'nivarana.org',
+                port: '',
             }
         ]
     }
