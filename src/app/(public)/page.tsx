e6e7e5a7c595551cdb20cb8c7ddf1759cde6c4fd
Akshay S Dinesh <akshay@learnlearn.in>
2025-02-16 10:06:26 +0530

    feat: hindi button

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,6 +2,7 @@ import ArticlePreview from "@/components/article/ArticlePreview";
 import LoadMore from "@/components/home/LoadMore";
 import Sidebar from "@/components/sidebar";
 import { getArticlesPaginated } from "@/data/cms";
+import Link from "next/link";
 
 export default async function Home() {
     const initialPosts = await getArticlesPaginated(0);
@@ -9,6 +10,14 @@ export default async function Home() {
         <div className="max-w-screen-xl mx-auto">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-4">
                 <div className="lg:col-span-2">
+                    <div className="flex justify-center mt-4">
+                        <Link
+                            href="/hindi"
+                            className="px-4 py-2 bg-blue-500 text-white rounded"
+                        >
+                            हिंदी
+                        </Link>
+                    </div>
                     {initialPosts.map((item, index) => (
                         <ArticlePreview
                             {...item}
