48d425f39223b47908e6b6364d1981dde93a7daa
Akshay S Dinesh <akshay@learnlearn.in>
2025-03-16 21:30:09 +0530

    fix: make author image appear normal

--- src/app/(public)/author/[slug]/page.tsx
+++ src/app/(public)/author/[slug]/page.tsx
@@ -52,12 +52,13 @@ export async function generateMetadata(
 function AuthorPic({ image }) {
     if (image === null) return;
     return (
-        <div className="col-md-2 col-sm-2 thumb d-flex justify-content-center align-items-center">
+        <div className="">
             <Image
                 src={getImageURLFromFileName(image)}
                 className="author"
                 alt="author"
-                fill={true}
+                height={400}
+                width={200}
             />
         </div>
     );
@@ -67,31 +68,23 @@ async function AuthorDetails({ data }) {
     return (
         <div className="mr-auto p-4 mb-40">
             <AuthorPic image={data.image} />
-            <div
-                className={
-                    data.image != null
-                        ? "col-md-10 col-sm-10 details"
-                        : "col-md-12 col-sm-12 details"
-                }
-            >
-                <h4 className="name mb-0 text-6xl mb-12">{data.name}</h4>
-                {data.title != null && (
-                    <p
-                        className="mt-2 max-w-prose"
-                        dangerouslySetInnerHTML={{
-                            __html: data.title,
-                        }}
-                    />
-                )}
-                {data.description != null && (
-                    <p
-                        className="mt-2 max-w-prose text-black"
-                        dangerouslySetInnerHTML={{
-                            __html: data.description,
-                        }}
-                    />
-                )}
-            </div>
+            <h4 className="name mb-0 text-6xl mb-12">{data.name}</h4>
+            {data.title != null && (
+                <p
+                    className="mt-2 max-w-prose"
+                    dangerouslySetInnerHTML={{
+                        __html: data.title,
+                    }}
+                />
+            )}
+            {data.description != null && (
+                <p
+                    className="mt-2 max-w-prose text-black"
+                    dangerouslySetInnerHTML={{
+                        __html: data.description,
+                    }}
+                />
+            )}
         </div>
     );
 }
