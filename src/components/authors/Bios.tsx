fc10c72ad5ae669cd3f9de39a06e72f721a17193
Akshay S Dinesh <akshay@learnlearn.in>
2025-07-19 15:48:58 +0530

    feat: tailwind styles for author bios

--- src/components/authors/Bios.tsx
+++ src/components/authors/Bios.tsx
@@ -1,49 +1,26 @@
-import { getImageURLFromFileName } from "@/utils/paths";
-import Image from "next/image";
 import Link from "next/link";
 
 export function NAuthorsBio({ authors }) {
     return (
-        <>
+        <div className="flex flex-col gap-1">
             {authors.map((a) => (
                 <AuthorBio {...a} key={a.id} />
             ))}
-        </>
+        </div>
     );
 }
-export function AuthorBio({ image, path, name, description }) {
-    const showImage = false;
+export function AuthorBio({ path, name, description }) {
     return (
-        <div className="about-author my-4 padding-30 rounded-sm row">
-            {image != null && showImage && (
-                <div className="col-md-2 col-sm-2 thumb d-flex justify-content-center align-items-center">
-                    <Image
-                        src={getImageURLFromFileName(image)}
-                        fill={true}
-                        className="author"
-                        alt="author"
-                    />
-                </div>
-            )}
-            <div
-                className={
-                    image != null
-                        ? "col-md-10 col-sm-10 details"
-                        : "col-md-12 col-sm-12 details"
-                }
-            >
-                <h4 className="name mb-0">
-                    <Link href={"/author/" + path}>{name}</Link>
-                </h4>
-                {description != null && (
+        <Link href={"/author/" + path}>
+            <div className="bg-nivarana-white/50 md:mx-4 rounded">
+                <div className="p-2 md:p-8 lg:p-16 md:max-w-md md:mx-auto">
+                    <div className="text-xl">{name}</div>
                     <div
-                        className="mt-2"
-                        dangerouslySetInnerHTML={{
-                            __html: description,
-                        }}
-                    />
-                )}
+                        className=""
+                        dangerouslySetInnerHTML={{ __html: description }}
+                    ></div>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
