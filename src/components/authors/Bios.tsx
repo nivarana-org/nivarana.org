b411f796c24681ddacbadb639e629bed13842e03
Akshay S Dinesh <akshay@learnlearn.in>
2025-07-20 22:24:36 +0530

    feat: let authors take up less space

--- src/components/authors/Bios.tsx
+++ src/components/authors/Bios.tsx
@@ -13,7 +13,7 @@ export function AuthorBio({ path, name, description }) {
     return (
         <Link href={"/author/" + path}>
             <div className="bg-nivarana-white/50 md:mx-4 rounded">
-                <div className="p-2 md:p-8 lg:p-16 md:max-w-3xl md:mx-auto">
+                <div className="p-2 md:max-w-3xl md:mx-auto">
                     <div className="text-xl text-nivarana-green">{name}</div>
                     <div
                         className=""
