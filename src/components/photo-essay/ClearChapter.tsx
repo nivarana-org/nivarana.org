5c8900343e66fb8d03f36bb3ade9a54730b26509
Akshay S Dinesh <akshay@learnlearn.in>
2025-10-10 21:14:50 +0530

    feat: remove unnecessary effect dependency

--- src/components/photo-essay/ClearChapter.tsx
+++ src/components/photo-essay/ClearChapter.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useEffectEvent } from "react";
 
 export default function ClearChapter() {
     const pathname = usePathname();
     const searchParams = useSearchParams();
-    useEffect(() => {
+    const clearAddressBar = useEffectEvent(() => {
         const newUrl = pathname;
         window.history.replaceState(null, "", newUrl);
+    });
+    useEffect(() => {
+        clearAddressBar();
     }, [searchParams]);
     return null;
 }
