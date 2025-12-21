d1697544f0a4310eb4edcbf32bea1e06b5d33451
Akshay S Dinesh <akshay@learnlearn.in>
2025-12-21 20:26:39 +0530

    feat: add database for better-auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,18 @@
 import { betterAuth } from "better-auth";
 import { customSession } from "better-auth/plugins";
+import { createPool } from "mysql2/promise";
 import { headers } from "next/headers";
 
 export const auth = betterAuth({
+    database: createPool({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DATABASE,
+        charset: "utf8mb4",
+        connectionLimit: 2,
+    }),
     plugins: [
         customSession(async ({ user, session }) => {
             const role = process.env.ADMINS?.includes(user.email)
